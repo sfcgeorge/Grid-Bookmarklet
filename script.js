aa5b0d4857969be8b8c54a34921a609c89b80435
Simon George <simon@sfcgeorge.co.uk>
2011-08-01 23:23:11 +0100

    Now working using the GitHub raw URLs. After the bookmarklet is dragged to the bookmarks bar, it will keep itself updated.

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-/*
- * jQuery Bookmarklet - version 1.0
- * Originally written by: Brett Barros
- * With modifications by: Paul Irish
- *
- * If you use this script, please link back to the source
- *
- * Copyright (c) 2010 Latent Motion (http://latentmotion.com/how-to-create-a-jquery-bookmarklet/)
- * Released under the Creative Commons Attribution 3.0 Unported License,
- * as defined here: http://creativecommons.org/licenses/by/3.0/
- *
- */
- 
 window.bookmarklet = function(opts){fullFunc(opts)};
  
 // These are the styles, scripts and callbacks we include in our bookmarklet:
@@ -165,4 +152,54 @@ window.bookmarklet({
    	}
 })
  
-function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}$.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>").attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js";(function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
+function fullFunc(opts){
+ 
+	// User doesn't have to set jquery, we have a default.
+	opts.jqpath = opts.jqpath || "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js";
+ 
+	function getJS(jsfiles){
+ 
+	// Check if we've processed all of the JS files (or if there are none)
+	if (jsfiles.length === 0) {
+		opts.ready();
+		return false;
+	}
+ 
+		// Load the first js file in the array
+		$.getScript(jsfiles[0],  function(){ 
+ 
+			// When it's done loading, remove it from the queue and call the function again    
+			getJS(jsfiles.slice(1));
+ 
+		})
+ 
+	}
+ 
+	// Synchronous loop for css files
+	function getCSS(csfiles){
+		$.each(csfiles, function(i, val){
+			$('<style />').load(val).appendTo('head');
+		});
+	}
+ 
+	function getjQuery(filename) {
+ 
+		// Create jQuery script element
+		var fileref = document.createElement('script')
+		fileref.type = 'text/javascript';
+		fileref.src =  filename;
+ 
+		// Once loaded, trigger other scripts and styles
+		fileref.onload = function(){
+ 
+			getCSS(opts.css); // load CSS files
+			getJS(opts.js); // load JS files
+ 
+		};
+ 
+		document.body.appendChild(fileref);
+	}
+ 
+	getjQuery(opts.jqpath); // kick it off
+ 
+}; // end of bookmarklet();
